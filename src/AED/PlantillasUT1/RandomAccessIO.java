d7ee6cc5a8344fc03044f1ddbe354a3d5e812584
xZ3NER <peng.asiacita@gmail.com>
2022-10-07 16:37:28 +0100

    RandomAccessIO para AED hecho + comentado + ejemplo con pruebas en Main

--- src/AED/PlantillasUT1/RandomAccessIO.java
+++ src/AED/PlantillasUT1/RandomAccessIO.java
@@ -3,8 +3,11 @@ package AED.PlantillasUT1;
 import java.io.*;
 import java.util.ArrayList;
 
+//Si da error, borrar el fichero creado y ejecutar de nuevo. (puede dar error al escribir, ya que no sobreescribe)
+//Ejemplo de utilización en TesteoFuncionamiento/MainPruebas.java
 public class RandomAccessIO {
 
+    //Tamaño por defecto de bytes.
     public enum BYTES {
         INT(4),
         CHAR(2),
@@ -27,6 +30,9 @@ public class RandomAccessIO {
         this.path = path;
     }
 
+    /**
+     * Inicia el proceso de escritura.
+     */
     public void initWriter() {
         try {
             this.randomAccessFile = new RandomAccessFile(path, "rw");
@@ -35,14 +41,21 @@ public class RandomAccessIO {
         }
     }
 
+    /**
+     * Escribe en el fichero el tipo de dato pasado por parámetro.
+     * (Requiere initWriter al principio y closeWriter al terminar)
+     *
+     * @param dataType  Indicar el tipo de dato.
+     * @param dataValue Indicar el valor del dato.
+     * @param <T>       Permite cualquier tipo de dato, pero tiene q concordar con el tipo de BYTES. (INT, 6)
+     */
     public <T> void writeFile(BYTES dataType, T dataValue) {
         try {
-
             switch (dataType) {
                 case INT -> this.randomAccessFile.writeInt((Integer) dataValue);
                 case CHAR -> this.randomAccessFile.writeChar((Character) dataValue);
                 case STRINGBUFFER -> {
-                    StringBuffer stringBuffer = new StringBuffer(dataValue.toString());
+                    StringBuilder stringBuffer = new StringBuilder(dataValue.toString());
                     stringBuffer.setLength(BYTES.STRINGBUFFER_LENGTH.byteSize);
 
                     this.randomAccessFile.writeChars(stringBuffer.toString());
@@ -59,28 +72,71 @@ public class RandomAccessIO {
         }
     }
 
+    /**
+     * Escribe en una posición concreta, el tipo de dato pasado por parámetro. (Para sobreescribir datos)
+     * (Requiere initWriter al principio y closeWriter al terminar)
+     *
+     * @param dataType  Indicar el tipo de dato.
+     * @param dataValue Indicar el valor del dato.
+     * @param <T>       Permite cualquier tipo de dato, pero tiene q concordar con el tipo de BYTES. (INT, 6)
+     * @param bytePos   Posición del dato a modificar/ lugar a escribir.
+     */
+    public <T> void writeFileByPos(BYTES dataType, T dataValue, int bytePos) {
+        try {
+            this.randomAccessFile.seek(bytePos);
+
+            switch (dataType) {
+                case INT -> this.randomAccessFile.writeInt((Integer) dataValue);
+                case CHAR -> this.randomAccessFile.writeChar((Character) dataValue);
+                case STRINGBUFFER -> {
+                    StringBuilder stringBuffer = new StringBuilder(dataValue.toString());
+                    stringBuffer.setLength(BYTES.STRINGBUFFER_LENGTH.byteSize);
+
+                    this.randomAccessFile.writeChars(stringBuffer.toString());
+                }
+                case DOUBLE -> this.randomAccessFile.writeDouble((Double) dataValue);
+            }
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Cierra el randomAccessFile.
+     */
     public void closeWriter() {
         try {
             this.randomAccessFile.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
-    public ArrayList<String> readAllFile(BYTES[] bytesTypes) {
+    /**
+     * Lee el contenido del fichero.
+     *
+     * @param bytesOrder Orden en el que fueron escritos los bytes.
+     * @return Lista con el contenido leido.
+     */
+    public ArrayList<String> readAllFile(BYTES[] bytesOrder) {
         ArrayList<String> dataList = new ArrayList<>();
-        int bytePos = 0, totalBytes = getTotalSize(bytesTypes);
+        int bytePos = 0, totalBytes = getTotalSize(bytesOrder);
 
         try {
             this.randomAccessFile = new RandomAccessFile(path, "r");
 
             while (!(randomAccessFile.getFilePointer() >= randomAccessFile.length())) {
-                //Dependiendo del tipo de dato, lo leerá con su correspondiente método.
-                readByType(bytesTypes, dataList);
 
-                bytePos+=totalBytes;
-                randomAccessFile.seek(bytePos);
+                //Dependiendo del tipo de dato, lo leerá con su correspondiente método.
+                readByType(bytesOrder, dataList);
+
+                bytePos += totalBytes;
+                this.randomAccessFile.seek(bytePos);
             }
 
             this.randomAccessFile.close();
@@ -94,7 +150,14 @@ public class RandomAccessIO {
         return dataList;
     }
 
-    public ArrayList<String> readByPosition(int initBytePos,BYTES[] bytesTypes) {
+    /**
+     * Lee un registro de un fichero, desde un lugar en concreto.
+     *
+     * @param initBytePos Posición desde donde quieras leer.
+     * @param bytesOrder  Orden en el que fueron escritos los bytes.
+     * @return Lista con el contenido leído.
+     */
+    public ArrayList<String> readByPosition(int initBytePos, BYTES[] bytesOrder) {
         ArrayList<String> dataList = new ArrayList<>();
         int bytePos = initBytePos;
 
@@ -102,10 +165,7 @@ public class RandomAccessIO {
             this.randomAccessFile = new RandomAccessFile(path, "r");
             this.randomAccessFile.seek(bytePos);
 
-            while (!(randomAccessFile.getFilePointer() >= randomAccessFile.length())) {
-                //Dependiendo del tipo de dato, lo leerá con su correspondiente método.
-                readByType(bytesTypes, dataList);
-            }
+            readByType(bytesOrder, dataList);
 
             this.randomAccessFile.close();
 
@@ -118,10 +178,17 @@ public class RandomAccessIO {
         return dataList;
     }
 
-    public int searchPositionById(String id,BYTES[] bytesTypes) {
+    /**
+     * Busca una posición en concreto, si coincide con el 'id'.
+     *
+     * @param id         String a buscar dentro del fichero.
+     * @param bytesTypes Orden en el que fueron escritos los bytes.
+     * @return Array de int, [0] = posición del registro / [1] = posición concreta.
+     */
+    public int[] searchPositionById(String id, BYTES[] bytesTypes) {
 
         int bytePos = 0, totalBytes = getTotalSize(bytesTypes);
-        int idPosition = 0;
+        int[] idPosition = new int[2];
 
         try {
             this.randomAccessFile = new RandomAccessFile(path, "r");
@@ -132,27 +199,42 @@ public class RandomAccessIO {
                 for (BYTES type : bytesTypes) {
                     String auxId = "";
 
+                    //Debido al desplazamiento al leer, al final habrá que restarle una cantidad de bytes
+                    //concreta para tener la posición exacta de la concordancia.
+                    int auxPosFixer = 0;
+
                     switch (type) {
-                        case INT -> auxId = String.valueOf(this.randomAccessFile.readInt());
-                        case CHAR -> auxId = String.valueOf(this.randomAccessFile.readChar());
+                        case INT -> {
+                            auxId = String.valueOf(this.randomAccessFile.readInt());
+                            auxPosFixer = BYTES.INT.byteSize;
+                        }
+                        case CHAR -> {
+                            auxId = String.valueOf(this.randomAccessFile.readChar());
+                            auxPosFixer = BYTES.CHAR.byteSize;
+                        }
                         case STRINGBUFFER -> {
                             char[] string = new char[BYTES.STRINGBUFFER_LENGTH.byteSize];
 
-                            for (int i = 0;i<string.length;i++) {
+                            for (int i = 0; i < string.length; i++) {
                                 string[i] = this.randomAccessFile.readChar();
                             }
 
                             auxId = new String(string).trim();
+                            auxPosFixer = BYTES.STRINGBUFFER.byteSize;
                         }
-                        case DOUBLE -> auxId = String.valueOf(this.randomAccessFile.readDouble());
+                        case DOUBLE -> {
+                            auxId = String.valueOf(this.randomAccessFile.readDouble());
+                            auxPosFixer = BYTES.DOUBLE.byteSize;
+                        }
                     }
 
                     if (auxId.equals(id)) {
-                        idPosition = (int) randomAccessFile.getFilePointer();
+                        idPosition[0] = bytePos;
+                        idPosition[1] = (int) this.randomAccessFile.getFilePointer() - auxPosFixer;
                     }
                 }
 
-                bytePos+=totalBytes;
+                bytePos += totalBytes;
                 randomAccessFile.seek(bytePos);
             }
 
@@ -167,7 +249,15 @@ public class RandomAccessIO {
         return idPosition;
     }
 
+    /**
+     * Lee del fichero el dato dependiendo del tipo, y lo guarda en una lista.
+     *
+     * @param bytesTypes Orden en el que fueron escritos los bytes.
+     * @param dataList   Lista usada para almacenar los datos leidos.
+     * @throws IOException Control de excepciones.
+     */
     private void readByType(BYTES[] bytesTypes, ArrayList<String> dataList) throws IOException {
+
         for (BYTES type : bytesTypes) {
             switch (type) {
                 case INT -> dataList.add(String.valueOf(this.randomAccessFile.readInt()));
@@ -175,7 +265,7 @@ public class RandomAccessIO {
                 case STRINGBUFFER -> {
                     char[] string = new char[BYTES.STRINGBUFFER_LENGTH.byteSize];
 
-                    for (int i = 0;i<string.length;i++) {
+                    for (int i = 0; i < string.length; i++) {
                         string[i] = this.randomAccessFile.readChar();
                     }
 
@@ -186,6 +276,13 @@ public class RandomAccessIO {
         }
     }
 
+    /**
+     * Devuelve el tamaño total de un registro.
+     * Un registro = (Miguel, 21, Estudiante)
+     *
+     * @param allSizes Orden en el que fueron escritos los bytes.
+     * @return El tamaño total.
+     */
     private int getTotalSize(BYTES[] allSizes) {
         int totalSize = 0;
         for (BYTES size : allSizes) {
--- src/AED/PlantillasUT1/TesteoFuncionamiento/MainPruebas.java
+++ src/AED/PlantillasUT1/TesteoFuncionamiento/MainPruebas.java
@@ -2,24 +2,64 @@ package AED.PlantillasUT1.TesteoFuncionamiento;
 
 import AED.PlantillasUT1.RandomAccessIO;
 
+import java.util.ArrayList;
+
 
 public class MainPruebas {
 
     private static String path = "./src/AED/PlantillasUT1/TesteoFuncionamiento/";
+    //Instancia
+    private static RandomAccessIO randomAccessIO = new RandomAccessIO(path+"prueba.txt");
     public static void main(String[] args) {
-        RandomAccessIO randomAccessIO = new RandomAccessIO(path+"prueba.txt");
 
+        //Inicio de escritura
         randomAccessIO.initWriter();
+
+        //Escritura registro 1
         randomAccessIO.writeFile(RandomAccessIO.BYTES.CHAR,'H');
         randomAccessIO.writeFile(RandomAccessIO.BYTES.DOUBLE,7.66);
-        randomAccessIO.writeFile(RandomAccessIO.BYTES.INT,2);
         randomAccessIO.writeFile(RandomAccessIO.BYTES.INT,4);
+        randomAccessIO.writeFile(RandomAccessIO.BYTES.STRINGBUFFER,"Hola Mundo");
+
+        //Escritura registro2
+        randomAccessIO.writeFile(RandomAccessIO.BYTES.CHAR,'J');
+        randomAccessIO.writeFile(RandomAccessIO.BYTES.DOUBLE,8.02);
+        randomAccessIO.writeFile(RandomAccessIO.BYTES.INT,5);
+        randomAccessIO.writeFile(RandomAccessIO.BYTES.STRINGBUFFER,"Hola Mundo2");
+
+        //Cierre de escritura
         randomAccessIO.closeWriter();
 
-        for (String text: randomAccessIO.readAllFile(new RandomAccessIO.BYTES[]{RandomAccessIO.BYTES.CHAR, RandomAccessIO.BYTES.DOUBLE, RandomAccessIO.BYTES.INT, RandomAccessIO.BYTES.INT})){
+        //Orden en el que fueron escritos los datos de los registros
+        RandomAccessIO.BYTES[] bytes = new RandomAccessIO.BYTES[]{
+                RandomAccessIO.BYTES.CHAR,
+                RandomAccessIO.BYTES.DOUBLE,
+                RandomAccessIO.BYTES.INT,
+                RandomAccessIO.BYTES.STRINGBUFFER
+        };
+
+        //Lectura de tod0 el fichero.
+        ArrayList<String> list = randomAccessIO.readAllFile(bytes);
+        for (String text: list){
             System.out.println(text);
         }
 
+        //Obtención de posiciones del registro donde el id es 5
+        int[] idPos = randomAccessIO.searchPositionById(String.valueOf(5),bytes);
+
+        //Inicio de escritura
+        randomAccessIO.initWriter();
+
+        //Escribe/Sobreescribe en la posición pasada por parámetro
+        randomAccessIO.writeFileByPos(RandomAccessIO.BYTES.INT,3,idPos[1]);
+
+        //Cierre de escritura
         randomAccessIO.closeWriter();
+
+        //Lee el registro de una posición concreta
+        ArrayList<String> listByPos = randomAccessIO.readByPosition(idPos[0],bytes);
+        for (String text: listByPos){
+            System.out.println(text);
+        }
     }
 }
