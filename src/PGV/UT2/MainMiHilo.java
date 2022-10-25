cbbd9178499e9d9e36e09f1e5e5343c311c97073
xZ3NER Laptop <peng.asiacita@gmail.com>
2022-10-25 13:09:25 +0100

    Actividad de hilos PGV hecho, a falta de formatear y comentar.

---
+++ src/PGV/UT2/MainMiHilo.java
@@ -0,0 +1,15 @@
+package PGV.UT2;
+
+public class MainMiHilo {
+    public static void main(String[] args) throws InterruptedException {
+
+        int numHilos = 4;
+
+        for (int i = 0;i<numHilos;i++) {
+            MiHilo miHilo = new MiHilo(i);
+            miHilo.getThread().join();
+        }
+
+        System.out.println("Hilo principal terminado.");
+    }
+}
