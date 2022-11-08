4c6c12f9d2ad3869ffae8f440567cbcaca072ddb
xZ3NER Laptop <peng.asiacita@gmail.com>
2022-11-08 21:11:41 +0000

    Mini Implementación de MongoDB

---
+++ src/AED/UT2/MongoDB/Tests/Main.java
@@ -0,0 +1,23 @@
+package AED.UT2.MongoDB.Tests;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
+        mongoLogger.setLevel(Level.SEVERE);
+
+        String connectionString = "mongodb://localhost:27017";
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+            databases.forEach(db -> System.out.println(db.toJson()));
+        }
+    }
+}
