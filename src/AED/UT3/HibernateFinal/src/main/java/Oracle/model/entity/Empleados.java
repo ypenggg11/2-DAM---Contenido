7627beb0f4b2fbc145980240e2578fdde090cc75
xZ3NER Laptop <peng.asiacita@gmail.com>
2023-01-13 08:59:57 +0000

    TODO HiberniteFinal

---
+++ src/AED/UT3/HibernateFinal/src/main/java/Oracle/model/entity/Empleados.java
@@ -0,0 +1,110 @@
+package Oracle.model.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigInteger;
+
+@Entity
+public class Empleados {
+    @Id
+    @Column(name = "ID_EMP")
+    private BigInteger idEmp;
+    @Basic
+    @Column(name = "NOMBRE_EMP")
+    private String nombreEmp;
+    @Basic
+    @Column(name = "SALARIO_EMP")
+    private double salarioEmp;
+    @Basic
+    @Column(name = "ID_DPT_EMP")
+    private BigInteger idDptEmp;
+    @ManyToOne
+    @JoinColumn(name = "ID_DPT_EMP", referencedColumnName = "ID_DPT", nullable = false,insertable=false, updatable=false)
+    private Departamentos departamentosByIdDptEmp;
+
+    public Empleados(){}
+    public Empleados(BigInteger idEmp, String nombreEmp, double salarioEmp, BigInteger idDptEmp) {
+        this.idEmp = idEmp;
+        this.nombreEmp = nombreEmp;
+        this.salarioEmp = salarioEmp;
+        this.idDptEmp = idDptEmp;
+    }
+
+    public BigInteger getIdEmp() {
+        return idEmp;
+    }
+
+    public void setIdEmp(BigInteger idEmp) {
+        this.idEmp = idEmp;
+    }
+
+    public String getNombreEmp() {
+        return nombreEmp;
+    }
+
+    public void setNombreEmp(String nombreEmp) {
+        this.nombreEmp = nombreEmp;
+    }
+
+    public double getSalarioEmp() {
+        return salarioEmp;
+    }
+
+    public void setSalarioEmp(double salarioEmp) {
+        this.salarioEmp = salarioEmp;
+    }
+
+    public BigInteger getIdDptEmp() {
+        return idDptEmp;
+    }
+
+    public void setIdDptEmp(BigInteger idDptEmp) {
+        this.idDptEmp = idDptEmp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Empleados empleados = (Empleados) o;
+
+        if (Double.compare(empleados.salarioEmp, salarioEmp) != 0) return false;
+        if (idEmp != null ? !idEmp.equals(empleados.idEmp) : empleados.idEmp != null) return false;
+        if (nombreEmp != null ? !nombreEmp.equals(empleados.nombreEmp) : empleados.nombreEmp != null) return false;
+        if (idDptEmp != null ? !idDptEmp.equals(empleados.idDptEmp) : empleados.idDptEmp != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = idEmp != null ? idEmp.hashCode() : 0;
+        result = 31 * result + (nombreEmp != null ? nombreEmp.hashCode() : 0);
+        temp = Double.doubleToLongBits(salarioEmp);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (idDptEmp != null ? idDptEmp.hashCode() : 0);
+        return result;
+    }
+
+    public Departamentos getDepartamentosByIdDptEmp() {
+        return departamentosByIdDptEmp;
+    }
+
+    public void setDepartamentosByIdDptEmp(Departamentos departamentosByIdDptEmp) {
+        this.departamentosByIdDptEmp = departamentosByIdDptEmp;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleados{" +
+                "idEmp=" + idEmp +
+                ", nombreEmp='" + nombreEmp + '\'' +
+                ", salarioEmp=" + salarioEmp +
+                ", idDptEmp=" + idDptEmp +
+                ", departamentosByIdDptEmp=" + departamentosByIdDptEmp +
+                '}';
+    }
+}
---
+++ src/AED/UT3/HibernateFinal/src/main/java/Oracle/model/orm/JPA_Manager.java
@@ -0,0 +1,114 @@
+package Oracle.model.orm;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JPA_Manager {
+
+    public enum Actions {
+        INSERT("INSERT"),
+        DELETE("DELETE"),
+        UPDATE("UPDATE"),
+        FIND("FIND");
+        Actions(String actionName){}
+    }
+    private final String PERSISTENCE_UNIT_NAME;
+    private EntityManagerFactory emf;
+    private EntityManager em;
+
+    public JPA_Manager(String persistenceUnitName) {
+        Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
+        this.PERSISTENCE_UNIT_NAME = persistenceUnitName;
+
+        try {
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+            em = emf.createEntityManager();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean insert(Object entity) {
+        boolean inserted;
+
+        em.getTransaction().begin();
+
+        try {
+            if (!(em.createQuery("from " + entity.getClass().getName()).getResultList().contains(entity))) {
+                em.persist(entity);
+                inserted = true;
+            } else {
+                inserted = false;
+            }
+        } catch (RuntimeException e) {
+            inserted = false;
+        }
+
+        em.getTransaction().commit();
+
+        return inserted;
+    }
+
+    public <T> List<T> getFullEntityList(Class<T> entityClass) throws RuntimeException {
+        return em.createQuery("from " + entityClass.getName(), entityClass).getResultList();
+    }
+
+    public <T, E> Object getEntityById(Class<T> entityClass, E entityId) throws RuntimeException {
+        return em.find(entityClass, entityId);
+    }
+
+    public <T, E> boolean removeEntityById(Class<T> entityClass, E entityId) {
+        boolean removed;
+
+        em.getTransaction().begin();
+
+        try {
+            em.remove(em.find(entityClass, entityId));
+            removed = true;
+        } catch (RuntimeException ignored) {
+            removed = false;
+        }
+
+        em.getTransaction().commit();
+
+        return removed;
+    }
+
+    public boolean updateEntity(Object newEntity) {
+        boolean updated;
+
+        em.getTransaction().begin();
+
+        try {
+            em.merge(newEntity);
+            updated = true;
+        }catch (RuntimeException e) {
+            updated = false;
+        }
+
+        em.getTransaction().commit();
+
+        return updated;
+    }
+
+    public static void printConfirmationMessage(boolean b, Actions action) {
+        if (b) {
+            System.out.println(action.name() + " successful!");
+        } else {
+            if (action == Actions.INSERT) {
+                System.out.println("Duplicated entry!");
+            } else {
+                System.out.println(action.name() + " failed!");
+            }
+        }
+    }
+
+    public void close() {
+        em.close();
+        emf.close();
+    }
+}
